cf657eb23b0747abf5116ff2f991dff23531951f
Viktor Zozuliuk <zozulyuk22@gmail.com>
2022-03-28 13:23:08 +0300

    Added the WordInText file

---
+++ BaseExercise/WordInText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace BaseExercise
+{
+    internal class WordInText
+    {
+        private List<(int, int)> _positions;
+
+        public string Word { get; private set; }
+
+        public int Frequency { get { return _positions.Count; } }
+
+        public List<(int, int)> Positions { get { return _positions; } }
+
+        public WordInText(string word, (int, int) position)
+        {
+            Word = word;
+            _positions = new List<(int, int)>() { position };
+        }
+
+        /// <summary>
+        /// Add a new position of the word that already exists in the dictionary.
+        /// </summary>
+        /// <param name="position">One more position of the word.</param>
+        public void AddPosition((int, int) position)
+        {
+            _positions.Add(position);
+        }
+    }
+}
+
