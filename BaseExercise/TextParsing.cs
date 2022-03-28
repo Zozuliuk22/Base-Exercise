64941567318d934ebf102e74e8bfc472d47dfc44
Viktor Zozuliuk <zozulyuk22@gmail.com>
2022-03-28 13:32:58 +0300

    Added the TextParsing file and the example text file

---
+++ BaseExercise/TextParsing.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseExercise
+{
+    internal static class TextParsing
+    {
+        /// <summary>
+        /// Form a dictionary from the text in the WordInText object format: for every word added its value and positions in the text.
+        /// </summary>
+        /// <param name="text">The text value.</param>
+        /// <returns>The formed word dictionary that is based on the text.</returns>
+        internal static Dictionary<string, WordInText> FormDictionary(string text)
+        {
+            var dictionary = new Dictionary<string, WordInText>();
+
+            text = ClearSpecialSymbols(text);
+            string[] lines = text.Split('\n');
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                string[] words = lines[i].Trim().ToLower().Split(' ');
+
+                for (var j = 0; j < words.Length; j++)
+                {
+                    if (!dictionary.ContainsKey(words[j]) && !String.IsNullOrEmpty(words[j]))
+                    {
+                        dictionary.Add(words[j], new WordInText(word: words[j], position: (i, j)));
+                    }
+                    else
+                    {
+                        dictionary[words[j]].AddPosition((i, j));
+                    }
+                }
+            }
+
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Remove from the text special symbols such as punctuation marks and open/close symbols.
+        /// </summary>
+        /// <param name="text">The text value.</param>
+        /// <returns>The text without special symbols.</returns>
+        internal static string ClearSpecialSymbols(string text)
+        {
+            char[] specialSymbols = { '.', ',', ':', ';', '"', '!', '?', '<', '>', '(', ')', '[', ']', '{', '}', '/', '\\', '|' };
+
+            foreach (var c in specialSymbols)
+                text = text.Replace(c.ToString(), "");
+
+            return text;
+        }
+    }
+}
+
